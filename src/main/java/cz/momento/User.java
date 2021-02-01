7b2f0f5418bc19b72131acfc8c6df5b56333f78c
Matyas Jokiel <matyas.jokiel1@gmail.com>
2021-02-01 14:14:21 +0100

    Added comments in User class

--- src/main/java/cz/momento/User.java
+++ src/main/java/cz/momento/User.java
@@ -11,12 +11,6 @@ public class User {
 
     ArrayList<Task> tasks = new ArrayList<>();
 
-    public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-
-    }
-
     public User() {
 
     }
@@ -39,42 +33,82 @@ public class User {
         return lastName;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Method adds a task
+     * @param task
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Method returns crypted login
+     * @return crypted login
+     */
     public String getCryptedLogin() {
         return this.cryptedLogin;
     }
 
+    /**
+     * Method sets crypted login
+     * @param cryptedLogin
+     */
     public void setCryptedLogin(String cryptedLogin) {
         this.cryptedLogin = cryptedLogin;
     }
 
+    /**
+     * Method sets first name
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Method sets last name
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Method returns user id
+     * @return user id
+     */
     public short getId() {
         return id;
     }
 
+    /**
+     * Method sets user id
+     * @param id
+     */
     public void setId(short id) {
         this.id = id;
     }
 
+    /**
+     * Method returns user table as string
+     * @return user table
+     */
     public String getUsrtbl() {
         return usrtbl;
     }
 
+    /**
+     * Method sets user table
+     * @param usrtbl
+     */
     public void setUsrtbl(String usrtbl) {
         this.usrtbl = usrtbl;
     }
